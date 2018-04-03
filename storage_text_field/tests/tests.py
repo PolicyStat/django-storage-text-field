e8aded4a23c6323bfb5293f186f3a7ca8b321d7d
Jason Ward <jason.ward@policystat.com>
2018-04-03 12:39:34 -0400

    Deal with HTML content being the same every time

--- storage_text_field/tests/tests.py
+++ storage_text_field/tests/tests.py
@@ -1,11 +1,19 @@
+import uuid
+
 from django.test import TestCase
 
 from storage_text_field.tests.models import Document
 
 
 class Foo(TestCase):
+    @property
+    def html(self):
+        return '<p>{}</p>'.format(uuid.uuid4())
+
     def test_smoke_test(self):
+        html = self.html
         document = Document.objects.create(
-            html='<p>foo</p>',
+            html=html,
         )
-        self.assertEqual(document.html, '<p>foo</p>')
+        document.refresh_from_db()
+        self.assertEqual(document.html, html)
