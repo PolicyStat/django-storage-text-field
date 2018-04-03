ab663f394bc1bcc2b8c68b2040252f48b3c299eb
Jason Ward <jason.ward@policystat.com>
2018-04-03 15:38:13 -0400

    Added new test models.

--- storage_text_field/tests/models.py
+++ storage_text_field/tests/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 from storage_text_field.fields import StorageTextField
+from storage_text_field.tests.storages import CustomerStorage
 
 
 class Document(models.Model):
     html = StorageTextField()
+
+
+class CustomerStorageDocument(models.Model):
+    html = StorageTextField(
+        storage='storage_text_field.tests.storages.CustomerStorage',
+    )
+
+
+class CustomerStorageObjectDocument(models.Model):
+    html = StorageTextField(
+        storage=CustomerStorage(),
+    )
