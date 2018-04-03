edac60ee02df32a14be92999544a5deee0904dfa
Jason Ward <jason.ward@policystat.com>
2018-04-03 14:36:26 -0400

    Added a test case checking the content in the DB.

--- storage_text_field/tests/tests.py
+++ storage_text_field/tests/tests.py
@@ -1,11 +1,13 @@
 import uuid
 
+from django.core.files.storage import default_storage
+from django.db import connection
 from django.test import TestCase
 
 from storage_text_field.tests.models import Document
 
 
-class Foo(TestCase):
+class SmokeTestCase(TestCase):
     @property
     def html(self):
         return '<p>{}</p>'.format(uuid.uuid4())
@@ -17,3 +19,19 @@ class Foo(TestCase):
         )
         document.refresh_from_db()
         self.assertEqual(document.html, html)
+
+    def test_db_is_saving_file_path_to_content(self):
+        html = self.html
+        document = Document.objects.create(
+            html=html,
+        )
+
+        cursor = connection.cursor()
+
+        query = 'SELECT html from tests_document WHERE id = %d'
+        cursor.execute(query % document.pk)
+        html_value = cursor.fetchone()[0]
+        self.assertEqual(
+            default_storage.open(html_value).read().decode('utf-8'),
+            html,
+        )
